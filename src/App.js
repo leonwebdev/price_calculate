fb36b4aad8f5b5d01ddfeaefdf3ce1a5e2e0b7e8
Leon J <leonjohnson377@gmail.com>
2024-04-25 07:27:50 -0400

    Add OneDigit component to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import OneDigit from "./OneDigit";
 import { Container, ListGroup } from "react-bootstrap";
 function App() {
     // ? ----------------------------------------------------
