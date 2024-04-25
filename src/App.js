a28bfc0e33416e2f85fe6047ff1a49f3a5f73890
Leon J <leonjohnson377@gmail.com>
2024-04-25 07:12:15 -0400

    Refactor App.js to update previous_close_price and loop range

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import "./App.css";
 import { useState } from "react";
 import { Container, Row, Col, ListGroup } from "react-bootstrap";
 function App() {
+    // ? ----------------------------------------------------
+    // * ----------------------------------------------------
+    // ! ----------------------------------------------------
     // ----------------------------------------------------
+    const previous_close_price = 11.81;
     // ----------------------------------------------------
-    const previous_close_price = 12.07;
-    // ----------------------------------------------------
-    // ----------------------------------------------------
-    let from = -3.1;
+    // ? ----------------------------------------------------
+    // * ----------------------------------------------------
+    // ! ----------------------------------------------------
+    let from = -2.1;
     let data = [];
-    for (let index = 0; index < 61; index++) {
+    for (let index = 0; index < 281; index++) {
         from += 0.1;
         data.push({
             percent: from,
@@ -20,7 +24,25 @@ function App() {
         <Container>
             <ListGroup variant="flush">
                 {data.map((item, index) => (
-                    <ListGroup.Item key={index}>
+                    <ListGroup.Item
+                        key={index}
+                        className={
+                            [0, 10].includes(index)
+                                ? "text-danger bg-black fw-bold"
+                                : [
+                                      30, 50, 60, 70, 80, 90, 100, 110, 120,
+                                      130, 140, 150, 160, 170, 180, 190, 200,
+                                      210, 220, 230, 240, 250, 260, 270, 280,
+                                      290,
+                                  ].includes(index)
+                                ? "text-danger bg-primary-subtle fw-bold"
+                                : [40].includes(index)
+                                ? "text-primary bg-danger-subtle fw-bold"
+                                : [20].includes(index)
+                                ? "text-primary bg-warning fw-bold"
+                                : ""
+                        }
+                    >
                         {item.percent
                             .toFixed(2)
                             .replace(/\d(?=(\d{3})+\.)/g, "$&,")}{" "}
