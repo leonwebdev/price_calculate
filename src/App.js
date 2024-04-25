1804a8c1286742e7301459874087fb142af6a190
Leon J <leonjohnson377@gmail.com>
2024-04-25 07:29:25 -0400

    Refactor App.js to add OneDigit component and update price display and styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import "./App.css";
 import OneDigit from "./OneDigit";
 import { Container, ListGroup } from "react-bootstrap";
-function App() {}
+function App() {
+    return (
+        <Container>
+            <OneDigit />
+        </Container>
+    );
+}
 
 export default App;
