d60a88531f0e3abfd11d597ddb0ec8b91efa08c4
Leon J <leonjohnson377@gmail.com>
2024-04-29 06:57:42 -0400

    Refactor App.js to add state for whichButton in App component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import OneDigit from "./OneDigit";
 import TwoDigit from "./TwoDigit";
 import { Container, Button } from "react-bootstrap";
 function App() {
+    const [whichButton, setWhichButton] = useState("one");
     return (
         <Container>
             {/* <OneDigit /> */}
