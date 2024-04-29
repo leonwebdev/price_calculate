34cd086588b82dfd20025cbf1884f88201f6303a
Leon J <leonjohnson377@gmail.com>
2024-04-29 07:06:15 -0400

    Update button variant in App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
                         return !prev;
                     })
                 }
-                variant="primary"
+                variant="danger"
                 style={{
                     position: "fixed",
                     bottom: "2rem",
