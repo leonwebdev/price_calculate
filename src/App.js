2e090b667a7a0d2e76467a2a1d12aa50477ea3bc
Leon J <leonjohnson377@gmail.com>
2024-04-29 07:11:06 -0400

    Update button padding in App.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
                     right: "2rem",
                     zIndex: 1000,width: "100px",
                     height: "100px",
-                    padding: 13px 18px,
+                    padding: "13px 18px",
                     border-radius: 60px,
                     font-size: 15px,
                     text-align: center,
