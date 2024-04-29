829dd157d15e9c2d724dcaa17628be7f7148706a
Leon J <leonjohnson377@gmail.com>
2024-04-29 07:11:24 -0400

    Update button border-radius in App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
                     zIndex: 1000,width: "100px",
                     height: "100px",
                     padding: "13px 18px",
-                    border-radius: 60px,
+                    borderRadius: "60px",
                     font-size: 15px,
                     text-align: center,
                 }}
